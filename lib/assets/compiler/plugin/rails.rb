af38a7a8f8ca1496bc8c3bbacbf46d534575095a
Jonathan Tropper <jonathan@ioffer.com>
2011-02-11 16:16:16 -0800

    Drop support for Rails < 2.3

--- lib/assets/compiler/plugin/rails.rb
+++ lib/assets/compiler/plugin/rails.rb
@@ -10,22 +10,13 @@ module Assets
             require 'assets/compiler/plugin/rack'
             Rails.configuration.middleware.use(Assets::Compiler::Plugin::Rack)
 
-            # Rails >= 2.3
+          # Rails >= 2.3
           elsif defined?(ActionController::Dispatcher) && defined?(ActionController::Dispatcher.middleware)
             require 'assets/compiler/plugin/rack'
             ActionController::Dispatcher.middleware.use(Assets::Compiler::Plugin::Rack)
 
           else
-            module ActionController
-              class Base
-                alias_method :sass_old_process, :process
-                def process(*args)
-                  Sass::Plugin.check_for_updates
-                  Assets::Compiler.compile!
-                  sass_old_process(*args)
-                end
-              end
-            end
+            raise StandardError, 'This version of rails is not supported'
           end
         end
 
